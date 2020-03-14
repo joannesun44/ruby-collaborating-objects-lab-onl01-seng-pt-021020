4886ced74e15fab0418b581f4c6d2d059c63665d
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-14 01:33:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,7 @@ class Artist
   end 
   
   def add_song(song)
-    @songs << song
+    # @songs << song
     song.artist = self
   end 
   
@@ -24,8 +24,7 @@ class Artist
   end 
   
   def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.create(name)
-    self.find(name) ? self.find(name) : self.new(name)
+    self.all.detect{|artist| artist.name == name} || self.new(name)
   end 
   
   def self.find(name)
